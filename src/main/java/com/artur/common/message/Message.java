3dbb693cabe977a36c48cb30b2584db9f6764a48
Artur <arttturyyy@gmail.com>
2024-08-20 22:05:00 +0200

    fix: added getter and setter in Message class

--- src/main/java/com/artur/common/message/Message.java
+++ src/main/java/com/artur/common/message/Message.java
@@ -1,10 +1,14 @@
 package com.artur.common.message;
 
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @ToString
 @EqualsAndHashCode
+@Getter
+@Setter
 public abstract class Message {
     protected Type type;
     protected Source source;
